58e6b3f5b307e05727c7bfbb5985e068af4b7de3
Aslan Socar <k.aslan17@gmail.com>
2024-12-04 14:51:23 +0400

    Minor Changes Performed

--- Program.cs
+++ Program.cs
@@ -7,47 +7,50 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        using (var rsa = new RSACryptoServiceProvider(2048)) 
+  
+
+        using (var rsa = new RSACryptoServiceProvider(2048))
         {
             rsa.PersistKeyInCsp = false;
 
             string publicKey = rsa.ToXmlString(false);
             string privateKey = rsa.ToXmlString(true);
 
-
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("Public key (available for every one):\n" + publicKey);
 
-
-
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("\n\nPrivate key (available on for me):\n" + privateKey);         
+            Console.WriteLine("\n\nPrivate key (available on for me):\n" + privateKey);
 
 
             //User 1 wants to send me a meessage using my public key:
             string messageFromUser1 = "Hello from User 1!";
-            string encryptedMessage = Encrypt(messageFromUser1, publicKey); 
-            Console.ForegroundColor= ConsoleColor.DarkYellow;
+            string encryptedMessage = Encrypt(messageFromUser1, publicKey);
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("\n\nUser 1 send an encrypted message:\n" + encryptedMessage);
 
             //I want to decrypt it using my private key
-            Console.ForegroundColor= ConsoleColor.Green;
             string decryptedMessage = Decrypt(encryptedMessage, privateKey);
+
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\n\nI decrypted the message:\n" + decryptedMessage);
 
 
             //I want to sign a message for User 2
-            string signature = Sign(decryptedMessage, privateKey);
-            Console.ForegroundColor= ConsoleColor.DarkBlue;
+            string message = "My sup3r secreT mes55a9e";
+
+            string signature = Sign(message, privateKey);
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("\n\nMy digital sign:\n" + signature);
 
             //User 2 wants to verify my signature
-            bool isSignatureValid = Verify(decryptedMessage, signature.Replace('a','c'), publicKey);
-            Console.ForegroundColor= ConsoleColor.DarkCyan;
-            Console.WriteLine("\n\nUser2 checks my signature...\n" + (isSignatureValid ? "Signature is valid" : "Signature is INVALID"));
+            bool isSignatureValid = Verify(message, signature, publicKey);
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\n\nUser2 is chechking my signature...\n" + (isSignatureValid ? "Signature is valid" : "Signature is INVALID"));
             Console.ResetColor();
-
-
         }
 
         #region Encryption | Decription
@@ -100,7 +103,5 @@ internal class Program
         }
 
         #endregion
-
-
     }
 }
